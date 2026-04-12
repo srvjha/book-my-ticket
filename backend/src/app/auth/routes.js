1e0332749fd3c2381b15ae0374eae8630add31ad
Saurav Jha <94675822+srvjha@users.noreply.github.com>
2026-04-12 21:03:12 +0530

    feat: implement authentication middleware, rate limiting, and frontend API service for booking system (#2)

--- backend/src/app/auth/routes.js
+++ backend/src/app/auth/routes.js
@@ -8,24 +8,27 @@ import {
   SignUpDto,
   SignInDto,
 } from "./models.js";
-
+import { authLimiter } from "../middleware/rateLimit.middleware.js";
 const authController = new AuthenticationController();
 const router = express.Router();
 
 router.post(
   "/signup",
   validate(SignUpDto),
+  authLimiter,
   authController.handleSignUp.bind(authController),
 );
 router.post(
   "/signin",
   validate(SignInDto),
+  authLimiter,
   authController.handleSignIn.bind(authController),
 );
 
 router.get(
   "/me",
   restrictToAuthenticatedUser(),
+  globalLimiter,
   authController.handleMe.bind(authController),
 );
 
---
+++ backend/src/app/middleware/rateLimit.middleware.js
@@ -0,0 +1,36 @@
+import { rateLimit } from 'express-rate-limit'
+
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100,
+   message: {
+    statusCode: 429,
+    message: "Too many requests. Please try again after 15 minutes.",
+    data: null,
+    success: false,
+  },
+});
+
+const authLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 5,
+   message: {
+    statusCode: 429,
+    message: "Too many requests. Please try again after 15 minutes.",
+    data: null,
+    success: false,
+  },
+});
+
+const bookingLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  limit: 20,
+   message: {
+    statusCode: 429,
+    message: "Too many requests. Please try again after 15 minutes.",
+    data: null,
+    success: false,
+  },
+});
+
+export { globalLimiter, authLimiter, bookingLimiter }
