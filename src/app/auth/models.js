2ab60b644d1744fdac18163071411d204c8258cc
Saurav Jha <srvtj629@gmail.com>
2026-04-12 16:40:00 +0530

    remove not required code

--- src/app/auth/models.js
+++ src/app/auth/models.js
@@ -18,47 +18,9 @@ export class SignInDto extends BaseDto {
   });
 }
 
-export class TokenDto extends BaseDto {
-  static schema = z.object({
-    token: z.string().max(4096),
-  });
-}
-
 export class EmailDto extends BaseDto {
   static schema = SignUpDto.schema.pick({
     email: true,
   });
 }
 
-export class ForgotPasswordDto extends BaseDto {
-  static schema = SignUpDto.schema.pick({
-    email: true,
-  });
-}
-
-export class ForgotPasswordVerifyDto extends BaseDto {
-  static schema = z.object({
-    newPassword: z
-      .string()
-      .min(8, "Password must be at least 8 characters long"),
-  });
-}
-
-export class ChangePasswordDto extends BaseDto {
-  static schema = z
-    .object({
-      oldPassword: z
-        .string()
-        .min(8, "Old password must be at least 8 characters long"),
-      newPassword: z
-        .string()
-        .min(8, "New password must be at least 8 characters long"),
-      confirmNewPassword: z
-        .string()
-        .min(8, "Confirm password must be at least 8 characters long"),
-    })
-    .refine((data) => data.newPassword === data.confirmNewPassword, {
-      message: "New password and confirm password do not match",
-      path: ["confirmNewPassword"],
-    });
-}
